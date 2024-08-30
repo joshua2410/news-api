d56d721dbd6cbe88aa918575946c27d06756ef11
Joshua Martin <martin.joshua2410@gmail.com>
2024-08-30 13:51:25 +0100

    completed post topic

--- models/news.models.js
+++ models/news.models.js
@@ -204,3 +204,15 @@ exports.sendArticle = (data) => {
         });
     });
 };
+
+exports.sendTopic = (data) => {
+  const { slug, description } = data;
+  return db
+    .query(`INSERT INTO topics (slug, description) VALUES($1,$2) RETURNING*`, [
+      slug,
+      description,
+    ])
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
